eae0aeb0c295d601f4897f83f42d4c6522ceb3db
Yashkulkarni205 <yashkulkarni205@gmail.com>
2021-03-20 19:05:29 +0530

    tutor exam and result added

--- tutorSearch/settings.py
+++ tutorSearch/settings.py
@@ -115,7 +115,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'jdmr',
         'USER': 'postgres',
-        'PASSWORD': 'Yashkul@205',
+        'PASSWORD': '',
         'HOST':'localhost',
         'PORT':'5432',
     },
