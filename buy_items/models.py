ce9d8aef05d9d972d2121329b40fe75e1190d661
sandeep agrawal <sandeepagrawal8875@gmail.com>
2021-03-26 11:38:57 +0530

    tutorial-buy-added

--- buy_items/models.py
+++ buy_items/models.py
@@ -2,7 +2,7 @@ import uuid
 
 from django.db import models
 from accounts.models import Student
-from tutorials.models import TutorialInstitute
+from tutorials.models import TutorialInstitute, TutorialTutors
 from notes.models import NotesInstitute, NotesTutor
 
 class BuyInstituteNotes(models.Model):
@@ -31,12 +31,11 @@ class BuyTutorNotes(models.Model):
     def __str__(self):
         return str(self.note.title) + str(self.student.id)
 
-class BuyTutorial(models.Model):
+class BuyTutorTutorial(models.Model):
     student = models.ForeignKey(to=Student, on_delete=models.CASCADE, null=True)
-    tutorial = models.ForeignKey(to=TutorialInstitute, on_delete=models.CASCADE, null=True)
+    tutorial = models.ForeignKey(to=TutorialTutors, on_delete=models.CASCADE, null=True)
     status = models.BooleanField(default=0 ,null=True)
     buy_at = models.DateField(auto_now_add=True)
-    amount = models.IntegerField(default=0, null=True)
     order_id = models.UUIDField(default=uuid.uuid4, unique=True, db_index=True, editable=False)
 
     class Meta:
@@ -44,5 +43,17 @@ class BuyTutorial(models.Model):
 
     def __str__(self):
         return str(self.tutorial.Title) + str(self.student.id)
-    
-    
+
+
+class BuyInstituteTutorial(models.Model):
+    student = models.ForeignKey(to=Student, on_delete=models.CASCADE, null=True)
+    tutorial = models.ForeignKey(to=TutorialInstitute, on_delete=models.CASCADE, null=True)
+    status = models.BooleanField(default=0 ,null=True)
+    buy_at = models.DateField(auto_now_add=True)
+    order_id = models.UUIDField(default=uuid.uuid4, unique=True, db_index=True, editable=False)
+
+    class Meta:
+        unique_together = [['tutorial', 'student']]
+
+    def __str__(self):
+        return str(self.tutorial.Title) + str(self.student.id)
--- buy_items/urls.py
+++ buy_items/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import buyInstituteNotes,buyTutorNotes
+from .views import buyInstituteNotes,buyTutorNotes, buyInstituteTutorial, buyTutorTutorial
+
 
 urlpatterns = [
     path('buyInstituteNotes/<int:id>', buyInstituteNotes, name='buyInstituteNotes'),
     path('buyTutorNotes/<int:id>', buyTutorNotes, name='buyTutorNotes'),
+    path('buyInstituteTutorial/<int:id>', buyInstituteTutorial ,name='buyInstituteTutorial'),
+    path('buyTutorTutorial/<int:id>', buyTutorTutorial ,name='buyTutorTutorial'),
 ]
