b8df3f355a45d94df38029b0b51ce5f1cdd3c804
Yashkulkarni205 <yashkulkarni205@gmail.com>
2021-04-03 16:26:44 +0530

    heroku

--- staticfiles/plugins/WebViewer/samples/global.90ffea8bea53.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable-next-line no-unused-vars */
-function samplesSetup(instance) {
-  instance.enableElements(['bookmarksPanel', 'bookmarksPanelButton']);
-  instance.enableFeatures([instance.Feature.Measurement]);
-}
