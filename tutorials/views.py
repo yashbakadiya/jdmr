de90764c7b0be6beb1ac124b97e9e43245bd63a6
sandeep agrawal <sandeepagrawal8875@gmail.com>
2021-04-09 09:46:29 +0530

    -

--- tutorials/views.py
+++ tutorials/views.py
@@ -533,7 +533,6 @@ def SearchCourses(request):
                 course = ""
             else:
                 course = courseName[0]
-            print('----------->',fees)
 
             prefill = {
                 "duration":duration,
