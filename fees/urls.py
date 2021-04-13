a56d3e25cef24313bbcb558987098276ae8347b5
Rakesh10021997 <rakesh832228@gmail.com>
2021-04-13 12:32:21 +0530

    bugs fix

--- fees/urls.py
+++ fees/urls.py
@@ -7,11 +7,12 @@ urlpatterns = [
     path('viewFees/', viewFees, name='viewFees'),
     path('submitFee/', submitFee, name='submitFee'),
     path('deleteFee/<int:id>', deleteFee, name='deleteFee'),
-    path('editFee/<int:id>', editFee, name='editFee'),
-    path('editFeef/<int:id>',editFeef,name='editFeef'),
+   
     path('archiveFeefirst/<int:id>', archiveFeefirst, name='archiveFeefirst'), 
     path('unarchiveFee/<int:id>', unarchiveFee, name='unarchiveFee'), 
     path('archiveFeeList/', archiveFeeList, name='archiveFeeList'),
     path('fees-api', AddFeesAPI, name='fees-api'),
     path('submitFee-api', SubmitFeesAPI, name='submitFee-api'),
+    path('editFee/<int:id>', editFee, name='editFee'),
+    path('editFeef/<int:id>',editFeef,name='editFeef'),
 ]
