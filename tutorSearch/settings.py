3a43395733f52850bb36eba5a7a2dacd64529169
Yashkulkarni205 <yashkulkarni205@gmail.com>
2021-04-20 18:04:54 +0530

    done profileupdate

--- tutorSearch/settings.py
+++ tutorSearch/settings.py
@@ -102,22 +102,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'tutorSearch.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'jdmr',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Yashkul@205',
-#         'HOST':'localhost',
-#         'PORT':'5432',
-#         'HOST':'localhost',
-#     }
-# }
-
-
 
 
 DATABASES = {
@@ -125,7 +109,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'jdmr',
         'USER': 'postgres',
-        'PASSWORD': 'Yashkul@205',
+        'PASSWORD': '',
         'HOST':'localhost',
         'PORT':'5432',
     },
@@ -218,9 +202,9 @@ REST_FRAMEWORK = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'yashkulkarni205@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_MAIL_SUBJECT = "COACHING OTP"
-EMAIL_HOST_PASSWORD = 'hlaliaitgolylcmd'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PAGE_DOMAIN = 'localhost'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
