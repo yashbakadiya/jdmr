5245136175b29d9cdaa3936eeefd6045e555a5ab
Yashkulkarni205 <yashkulkarni205@gmail.com>
2021-05-19 17:10:41 +0530

    fix

--- tutorSearch/settings.py
+++ tutorSearch/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'jdmr',
         'USER': 'postgres',
-        'PASSWORD': 'Sandeep@99',
+        'PASSWORD': 'Yashkul@205',
         'HOST':'localhost',
         'PORT':'5432',
     },
