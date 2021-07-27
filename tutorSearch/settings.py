2a4f2e82ba55021d716b29f5219343cce5752ae9
Yash <bakadiyayash@gmail.com>
2021-07-27 15:49:41 +0530

    commit

--- tutorSearch/settings.py
+++ tutorSearch/settings.py
@@ -16,7 +16,6 @@ from decouple import config
 from ckeditor.configs import DEFAULT_CONFIG
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# git remote add heroku https://git.heroku.com/final-jdmr.git
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
